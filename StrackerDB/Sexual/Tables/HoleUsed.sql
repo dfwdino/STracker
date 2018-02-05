bac188fe42fb2d19ca2a8b3031ff3e33fbb9bb41
shane newsom <shane.newsom@gmail.com>
2018-02-05 16:20:04 -0600

    DB update

--- StrackerDB/Sexual/Tables/HoleUsed.sql
+++ StrackerDB/Sexual/Tables/HoleUsed.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [Sexual].[HoleUsed] (
-    [ID]      INT NOT NULL,
+    [ID]      INT IDENTITY (1, 1) NOT NULL,
     [HoleID]  INT NOT NULL,
     [Deleted] BIT CONSTRAINT [DF_HoleUsed_Deleted] DEFAULT ((0)) NOT NULL,
-    [EventID] INT NULL,
+    [EventID] INT NOT NULL,
     CONSTRAINT [PK_HoleUsed] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_HoleUsed_Event] FOREIGN KEY ([EventID]) REFERENCES [Events].[Event] ([ID]),
     CONSTRAINT [FK_HoleUsed_Holes] FOREIGN KEY ([HoleID]) REFERENCES [Sexual].[Holes] ([ID])
 );
+
+
 
--- StrackerDB/Sexual/Tables/Holes.sql
+++ StrackerDB/Sexual/Tables/Holes.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [Sexual].[Holes] (
-    [ID]      INT           NOT NULL,
+    [ID]      INT           IDENTITY (1, 1) NOT NULL,
     [Area]    NVARCHAR (50) NOT NULL,
     [Deleted] BIT           CONSTRAINT [DF_Holes_Deleted] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_Holes] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
