c22e5077872a71ba7fd71289caf6564e05b03adb
shane newsom <shane.newsom@gmail.com>
2018-07-02 17:38:53 -0500

    Updated the DB and now showing the lastest entry.

---
+++ STracker/Models/AnswersListModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STracker.Models
+{
+    public class AnswersListModel
+    {
+        public string Name { get; set; }
+
+        public List<string> Questions { get; set; }
+        public List<string> Answer { get; set; }
+        public DateTime EventDate { get; set; }
+    }
+}
--- StrackerDB/People/Tables/STIREsults.sql
+++ StrackerDB/People/Tables/STIREsults.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [People].[STIREsults] (
-    [ID]       INT           IDENTITY (1, 1) NOT NULL,
-    [STI]      NVARCHAR (50) NOT NULL,
-    [Result]   NVARCHAR (50) NOT NULL,
-    [PersonID] INT           NULL,
-    [Deleted]  BIT           CONSTRAINT [DF_STIREsults_Deleted] DEFAULT ((0)) NOT NULL,
+    [ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [STI]         NVARCHAR (50) NOT NULL,
+    [Result]      NVARCHAR (50) NOT NULL,
+    [PersonID]    INT           NULL,
+    [Deleted]     BIT           CONSTRAINT [DF_STIREsults_Deleted] DEFAULT ((0)) NOT NULL,
+    [ResultsDate] DATE          NULL,
     CONSTRAINT [PK_STIREsults] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_STIREsults_People] FOREIGN KEY ([PersonID]) REFERENCES [People].[People] ([ID])
 );
+
+
 
 
 
