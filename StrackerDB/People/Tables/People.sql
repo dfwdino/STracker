ee9ff2723bf7410cd90253151eec6c34771573f0
shane newsom <shane.newsom@gmail.com>
2018-10-31 12:54:41 -0500

    Sytle Changes

--- StrackerDB/People/Tables/People.sql
+++ StrackerDB/People/Tables/People.sql
@@ -1,13 +1,17 @@
 ﻿CREATE TABLE [People].[People] (
-    [ID]      INT            IDENTITY (1, 1) NOT NULL,
-    [Name]    NVARCHAR (50)  NOT NULL,
-    [Notes]   NVARCHAR (MAX) NULL,
-    [Hide]    BIT            CONSTRAINT [DF_People_Hide] DEFAULT ((0)) NOT NULL,
-    [Deleted] BIT            CONSTRAINT [DF_People_Deleted] DEFAULT ((0)) NOT NULL,
-    [OwnerID] INT            NULL,
+    [ID]                  INT            IDENTITY (1, 1) NOT NULL,
+    [Name]                NVARCHAR (50)  NOT NULL,
+    [Notes]               NVARCHAR (MAX) NULL,
+    [Hide]                BIT            CONSTRAINT [DF_People_Hide] DEFAULT ((0)) NOT NULL,
+    [Deleted]             BIT            CONSTRAINT [DF_People_Deleted] DEFAULT ((0)) NOT NULL,
+    [OwnerID]             INT            NULL,
+    [WhereDidYouMeetThem] NVARCHAR (50)  NULL,
+    [PhotoURL]            NVARCHAR (255) NULL,
     CONSTRAINT [PK_People] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_People_Login] FOREIGN KEY ([OwnerID]) REFERENCES [Stracker].[Login] ([ID])
 );
+
+
 
 
 
